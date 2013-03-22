d4c3a29da6bff3e75c03e65ed57586e3021b36d3
wihoho <wihoho@gmail.com>
2013-03-22 15:55:10 +0800

    PerformanceTest

---
+++ src/Validation/PerformanceTest.java
@@ -0,0 +1,18 @@
+package Validation;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class PerformanceTest {
+	public static void main(String args[]){
+		Random randomGenerator = new Random();
+		while(true){
+			System.out.println(randomGenerator.nextInt(10));
+		}
+	}
+	
+	static ArrayList<String> generateTrainTestSet(int trainNum, int testNum){
+		ArrayList<Integer> alreadyInside = new ArrayList<Integer>();
+		return null;
+	}
+}
