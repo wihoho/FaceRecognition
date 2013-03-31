db0dcf1fd5248c571b3b6065c0dc2d0df08f3710
wihoho <wihoho@gmail.com>
2013-03-31 20:03:08 +0800

    Eigenfaces display

--- src/MainTestDriver.java
+++ src/MainTestDriver.java
@@ -7,8 +7,8 @@ import Jama.Matrix;
 public class MainTestDriver {
 	public static void main(String[] args){
 		try{				       		      	
-			Matrix s1 = FileManager.convertPGMtoMatrix("faces\\s3\\1.pgm");
-			Matrix s2 = FileManager.convertPGMtoMatrix("faces\\s3\\2.pgm");
+			Matrix s1 = FileManager.convertPGMtoMatrix("faces\\s1\\1.pgm");
+			Matrix s2 = FileManager.convertPGMtoMatrix("faces\\s1\\2.pgm");
 			Matrix s3 = FileManager.convertPGMtoMatrix("faces\\s2\\1.pgm");
 			Matrix s4 = FileManager.convertPGMtoMatrix("faces\\s2\\2.pgm");
 			
@@ -21,13 +21,14 @@ public class MainTestDriver {
 			trainSet.add(vectorize(s4));
 			
 			ArrayList<String> label = new ArrayList<String>();
-			label.add("s3");
-			label.add("s3");
+			label.add("s1");
+			label.add("s1");
 			label.add("s2");
 			label.add("s2");
 			
-			LLP pca = new LLP(trainSet, label, 2);
+			PCA pca = new PCA(trainSet, label, 2);
 			
+			Matrix eigen = normalize(pca.getW().getMatrix(0, 10303, 0, 0));
 			Matrix testCase = pca.getW().transpose().times(vectorize(testS3).minus(pca.meanMatrix));
 			Metric metric = new EuclideanDistance();
 			ArrayList<projectedTrainingMatrix> trainingSet =  pca.getProjectedTrainingSet();
@@ -46,11 +47,44 @@ public class MainTestDriver {
 		int n = input.getColumnDimension();
 		
 		Matrix result = new Matrix(m*n,1);
-		for(int p = 0; p < m; p ++){
-			for(int q = 0; q < n; q ++){
-				result.set(p*n+q, 0, input.get(p, q));
+		for(int p = 0; p < n; p ++){
+			for(int q = 0; q < m; q ++){
+				result.set(p*m+q, 0, input.get(q, p));
 			}
 		}
 		return result;
+	}
+	
+	static Matrix normalize(Matrix input){
+		int row = input.getRowDimension();
+		
+		for(int i = 0; i < row; i ++){
+			input.set(i, 0, 0-input.get(i, 0));
+			
+		}
+		
+		double max = input.get(0, 0);
+		double min = input.get(0, 0);
+		
+		for(int i = 1; i < row; i ++){
+			if(max < input.get(i,0))
+				max = input.get(i, 0);
+			
+			if(min > input.get(i, 0))
+				min = input.get(i, 0);
+			
+		}
+		
+		Matrix result = new Matrix(112,92);
+		for(int p = 0; p < 92; p ++){
+			for(int q = 0; q < 112; q ++){
+				double value = input.get(p*112+q, 0);
+				value = (value - min) *255 /(max - min);
+				result.set(q, p, value);
+			}
+		}
+		
+		return result;
+		
 	}
 }
